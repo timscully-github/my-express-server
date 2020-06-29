95649fe42feafc90a5849c04612d6b52e9afe15d
Tim Scully <tim.scully@wundermanthompson.com>
2020-06-29 17:06:17 +0100

    created a request for the server's get method

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const express = require("express");
 // create const to use express
 const app = express();
 
+// express method to tell server what to do when it makes a get request (console log request to see info associated with request)
+app.get("/", function(request, response) {
+  // send a response back for the get request to the browser
+  response.send("Hello");
+});
+
 // telling express to start a server (localhost:3000)
 app.listen(3000, function() {
   console.log("Server started on port 3000");
